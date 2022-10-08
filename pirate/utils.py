b143f46bd22833914606b7b8f013f29e23a69c4b
Omkaar <79257339+Infiniticity@users.noreply.github.com>
2022-10-08 09:53:33 +0530

    Store the Base URL

--- pirate/utils.py
+++ pirate/utils.py
@@ -1,14 +1,18 @@
 """
 MIT License
+
 Copyright (c) 2022 Omkaar
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,11 +28,14 @@ from __future__ import annotations
 from requests import get
 
 
+BASE_URL = "https://pirate.monkeyness.com/"
+
+
 def insult() -> str:
     """
     Generates a pirate insult.
     """
-    response = get("https://pirate.monkeyness.com/api/insult")
+    response = get(f"{BASE_URL}api/insult")
     return response.text
 
 
@@ -39,5 +46,5 @@ def translate(text: str) -> str:
     :param text: The text to translate.
     :type text: :class:`str`
     """
-    response = get("https://pirate.monkeyness.com/api/translate", params = {"english": text})
+    response = get(f"{BASE_URL}api/translate", params = {"english": text})
     return response.text
