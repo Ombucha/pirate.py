5311219711c1dbebbbbf557a98a0e2edb90040a1
Omkaar <omkaar.nerurkar@gmail.com>
2025-06-09 10:52:40 +0530

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,9 +7,9 @@ sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath(".."))
 
 on_rtd = os.environ.get("READTHEDOCS") == "True"
-project = "lithon"
-copyright = "2022, Omkaar"
-author = "Infiniticity"
-release = "1.0.1"
+project = "pirate.py"
+copyright = "2025, Omkaar"
+author = "Ombucha"
+release = "1.1.0"
 
 extensions = ["sphinx.ext.autodoc"]
